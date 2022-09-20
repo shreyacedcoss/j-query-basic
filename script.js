fc34a9220b5831adc528559b2447472df685fa2e
shreya <shreya@cedcoss.com>
2022-09-20 14:26:16 +0530

    task1

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+        $("#confirmPassword").keyup(checkPassword);
+     });
+    
+    function checkPassword() {
+    
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
+        console.log(password);
+        if (password != confirmPassword)
+            $("#output").html("Passwords do not match!").css("color", "red");
+        else
+            $("#output").html("Passwords match.").css("color", "green");
+    }
